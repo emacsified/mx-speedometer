2b00c2bf081c9f0b130e64c08e34975c19fa110d
Ash McBride <ash@ashmcbride.top>
2021-03-04 20:10:07 +0000

    fix(lint): Remove empty block, add hiding logic

--- src/client/client.ts
+++ src/client/client.ts
@@ -17,7 +17,7 @@ setInterval(() => {
     const currentVehicle = GetVehiclePedIsIn(player, false);
     // if player is in the driver's seat
     if (GetPedInVehicleSeat(currentVehicle, -1) == player) {
-      while (GetPedInVehicleSeat) {}
+      // while (GetPedInVehicleSeat) {}
       // turn on speedo, send data.
       const rpm = GetVehicleCurrentRpm(currentVehicle);
       const gear = GetVehicleCurrentGear(currentVehicle);
@@ -28,6 +28,9 @@ setInterval(() => {
         speed,
       };
       SendNuiMessage(JSON.stringify({ type: 'UIState', action: 'show', ...message }));
+    } else if (isOpen == true) {
+      SendNuiMessage(JSON.stringify({ type: 'UISTate', action: 'hide' }));
+      isOpen = false;
     }
   } else if (isOpen == true) {
     SendNuiMessage(JSON.stringify({ type: 'UISTate', action: 'hide' }));

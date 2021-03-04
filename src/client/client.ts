d4938d896d0d15ea9247e07fca1c921dffb5f793
Ash McBride <ash@ashmcbride.top>
2021-03-04 20:07:42 +0000

    Add speedo logic

--- src/client/client.ts
+++ src/client/client.ts
@@ -10,19 +10,31 @@ onNet('helloclient', message => {
 
 let isOpen = false;
 
-const toggleUI = () => {
-  isOpen = !isOpen;
-  SetNuiFocus(false, isOpen);
-  const msg = { type: 'UIToggle', action: 'toggle' };
-  const msgString = JSON.stringify(msg);
-  SendNuiMessage(msgString);
-};
+const player = PlayerPedId();
 
-setTick(() => {
-  if (IsControlJustReleased(0, 29)) {
-    toggleUI();
+setInterval(() => {
+  if (IsPedInAnyVehicle(player, false)) {
+    const currentVehicle = GetVehiclePedIsIn(player, false);
+    // if player is in the driver's seat
+    if (GetPedInVehicleSeat(currentVehicle, -1) == player) {
+      while (GetPedInVehicleSeat) {}
+      // turn on speedo, send data.
+      const rpm = GetVehicleCurrentRpm(currentVehicle);
+      const gear = GetVehicleCurrentGear(currentVehicle);
+      const speed = GetEntitySpeed(currentVehicle);
+      const message = {
+        rpm,
+        gear,
+        speed,
+      };
+      SendNuiMessage(JSON.stringify({ type: 'UIState', action: 'show', ...message }));
+    }
+  } else if (isOpen == true) {
+    SendNuiMessage(JSON.stringify({ type: 'UISTate', action: 'hide' }));
+    isOpen = false;
   }
-  if (IsControlJustReleased(0, 20) && isOpen) {
-    toggleUI();
-  }
-});
+}, 100);
+
+// setTick(() => {
+// if player is in a vehicle already
+// });
--- src/ui/src/containers/WindowListener/index.js
+++ src/ui/src/containers/WindowListener/index.js
@@ -11,13 +11,54 @@ const WindowListener = props => {
     };
   });
 
-  const { setHidden, hidden } = useStore(state => state);
+  const {} = useStore(state => state);
   const handleEvent = event => {
-    const { action, type } = event.data;
-    if (action == 'toggle' && type == 'UIToggle') {
-      setHidden(!hidden);
+    const data = event.data;
+    data.useMetric = false;
+
+    if (data.useMetric != undefined) {
+      const useMetricNow = data.useMetric != false;
+      if (useMetric != useMetricNow) {
+        useMetric = useMetricNow;
+        UnitDisplay.innerText = useMetric ? 'KMH' : 'MPH';
+      }
+    }
+
+    if (data.rawSpeed != undefined) {
+      var multiplier = useMetric ? 3.6 : 2.236936;
+      var unitSpeed = Math.floor(parseFloat(data.rawSpeed) * multiplier);
+      var speedString = unitSpeed.toString();
+
+      if (speedString.length > 3) speedString = '999';
+      console.log(speedString);
+
+      for (let i = 0; i < 3; i++) {
+        SpeedDisplay[i].innerText = speedString[i] == '&' ? '' : speedString[i];
+      }
+    }
+
+    if (data.gear != undefined) {
+      const gear = parseInt(data.gear);
+      console.log(gear);
+      if (gear == 0) {
+        GearNum.innerText = 'R';
+        GearDisplay.classList.add('reverseGear');
+        GearDisplay.classList.remove('normalGear');
+      } else {
+        GearNum.innerText = gear;
+        GearDisplay.classList.remove('reverseGear');
+        GearDisplay.classList.add('normalGear');
+      }
+    }
+
+    if (data.rpm != undefined) {
+      const rawRpm = parseFloat(data.rpm);
+      console.log(rpm);
+      RpmDisplay.style.width = `${(parseFloat(data.rpm) * 100.0).toFixed(2)}%`;
+      GearDisplay.classList.toggle('rpmOverload', rawRpm * 9 > 7.5);
     }
   };
+
   return <>{props.children}</>;
 };
 

4031d37ab128fc7904006f6e6aebfb91a768dc4a
Ash McBride <ash@ashmcbride.top>
2021-03-06 18:21:53 +0000

    fix: loop code

--- client.lua
+++ client.lua
@@ -1,23 +1,26 @@
 local player = PlayerPedId()
 
 CreateThread(function()
-    if IsPedInAnyVehicle(player, false) then
+    while true do
+      if IsPedInAnyVehicle(player, false) then
         local vehicle = GetVehiclePedIsIn(player, false);
         -- If driving, and the engine is running
         if GetPedInVehicleSeat(vehicle, -1) == player and GetIsVehicleEngineRunning(currentVehicle) == true then
-            local gear = GetVehicleCurrentGear(vehicle)
-            local speed = GetEntitySpeed(vehicle)
-            local fuel = GetVehicleFuelLevel(vehicle)
-            local isAirborne = IsPedInAnyPlane(player) or IsPedInAnyHeli(player)
-            SendNuiMessage({type = 'UIState', action = 'show', gear = gear, speed = speed, fuel = fuel, isAirborne = isAirborne})
-            Wait(100)
+          local gear = GetVehicleCurrentGear(vehicle)
+          local speed = GetEntitySpeed(vehicle)
+          local fuel = GetVehicleFuelLevel(vehicle)
+          local isAirborne = IsPedInAnyPlane(player) or IsPedInAnyHeli(player)
+          SendNuiMessage({type = 'UIState', action = 'show', gear = gear, speed = speed, fuel = fuel, isAirborne = isAirborne})
+          Wait(100)
         else
-            SendNuiMessage({type = "UIState", action = "hide"})
-            Wait(500)
+          SendNuiMessage({type = "UIState", action = "hide"})
+          Wait(500)
         end
-    else
+      else
         SendNuiMessage({type = "UIState", action = "hide"})
         Wait(500)
+      end
+
     end
 
 

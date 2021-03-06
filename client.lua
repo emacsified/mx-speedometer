f4648cb9a8b724a89e6c8affc44c70f853631fb0
Ash McBride <ash@ashmcbride.top>
2021-03-06 18:23:38 +0000

    feat: add logging

--- client.lua
+++ client.lua
@@ -2,10 +2,13 @@ local player = PlayerPedId()
 
 CreateThread(function()
     while true do
+      print("starting loop")
       if IsPedInAnyVehicle(player, false) then
+        print("is in a vehicle")
         local vehicle = GetVehiclePedIsIn(player, false);
         -- If driving, and the engine is running
         if GetPedInVehicleSeat(vehicle, -1) == player and GetIsVehicleEngineRunning(currentVehicle) == true then
+          print("is driving")
           local gear = GetVehicleCurrentGear(vehicle)
           local speed = GetEntitySpeed(vehicle)
           local fuel = GetVehicleFuelLevel(vehicle)
@@ -14,10 +17,12 @@ CreateThread(function()
           Wait(100)
         else
           SendNuiMessage({type = "UIState", action = "hide"})
+          print("in car, not driving")
           Wait(500)
         end
       else
         SendNuiMessage({type = "UIState", action = "hide"})
+        print("not in a car")
         Wait(500)
       end
 
